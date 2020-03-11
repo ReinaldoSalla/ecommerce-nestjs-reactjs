5d9029596978de4ebc2b5be66556659ee7f7c4c5
Reinaldo Borges Salla <reinaldo.salla@pampa.compasso>
2020-03-11 10:30:20 -0300

    First commit

---
+++ frontend/src/index.js
@@ -0,0 +1,41 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+export default class Interface extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = { isLoading: true };
+	}
+
+	componentDidMount() {
+		return fetch("https://reactnative.dev/movies.json")
+			.then(res => res.json())
+			.then(data => {
+				this.setState({
+					isLoading: false,
+					title: data.title,
+					description: data.title,
+					movies: data.movies
+				})
+			})
+			.catch(err => console.error(err));
+	}
+
+	render() {
+		if(this.state.isLoading) {
+			return (
+				<div>Loading</div>
+			)
+		} else {
+			return (
+				<div> first movie: {this.state.movies[0].title} </div>
+			)
+		}
+	}
+}
+
+ReactDOM.render(
+	<Interface />,
+	document.getElementById("root")
+);
+
