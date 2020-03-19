67d7e1de9135ee756b10144b0de0d147fa9da81e
Reinaldo Borges Salla <reinaldo.salla@pampa.compasso>
2020-03-19 10:44:10 -0300

    Added PATCH method

--- frontend/src/components/patch.js
+++ frontend/src/components/patch.js
@@ -33,18 +33,45 @@ export default class extends React.Component {
       release: this.state.release
     };
     fetch(`http://localhost:3000/api/products/${this.state.id}`, {
-    	method: "UPDATE",
-    	headers: {"contente-type": "application/json"},
-    	body: JSON.stringify(body)
+      method: "PATCH",
+      headers: {"content-type": "application/json"},
+      body: JSON.stringify(body)
     })
     .catch(err => console.error(err));
   }
 
   render() {
   	return (
-  		<div>
-  			<h1>Update one product</h1>
-  		</div>
+      <div>
+        <h1>Patch one product</h1>
+        <form onSubmit={this.onSubmit}>
+          <label>Product Id</label>
+          <br />
+          <input type="text" name="id" value={this.state.id} onChange={this.onChange} />
+          <br />
+          <label>Category</label>
+          <br />
+          <input type="text" name="category" value={this.state.category} onChange={this.onChange} />
+          <br />
+          <label>Subcategory</label>
+          <br />
+          <input type="text" name="subcategory" value={this.state.subcategory} onChange={this.onChange} />
+          <br />
+          <label>Name</label>
+          <br />
+          <input type="text" name="name" value={this.state.name} onChange={this.onChange} />
+          <br />
+          <label>Price</label>
+          <br />
+          <input type="number" name="price" value={this.state.price} onChange={this.onChange} min="0" step="0.1" />
+          <br />
+          <label>Amount</label>
+          <br />
+          <input type="number" name="amount" value={this.state.amount} onChange={this.onChange} min="0" step="1" />
+          <br /> <br />
+          <button type="submit">Patch</button>
+        </form>
+      </div>
   	);
   }
 }
