4356f0c5a8eb2d572135aeaa2682fc7b5e3820e2
Reinaldo Salla <reinaldosalla95@gmail.com>
2020-03-21 16:29:41 -0300

    Added validations for PATCH method

--- backend/src/products/dto/create-product.dto.ts
+++ backend/src/products/dto/create-product.dto.ts
@@ -6,11 +6,11 @@ import {
 } from "class-validator";
 
 export class CreateProductDto {
-  @MinLength(5) @MaxLength(10) category: string;
-  @MinLength(5) @MaxLength(10) subcategory: string;
-  @MinLength(5) @MaxLength(10) name: string;
-  @Min(1) @Max(100) price: number;
+  @MinLength(5) @MaxLength(50) category: string;
+  @MinLength(5) @MaxLength(50) subcategory: string;
+  @MinLength(5) @MaxLength(50) name: string;
+  @Min(1) @Max(1000000) price: number;
+  @Min(1) @Max(1000000) amount: number;
   available: boolean;
-  amount: number;
   release: Date;
 }
--- backend/src/products/dto/update-product.dto.ts
+++ backend/src/products/dto/update-product.dto.ts
@@ -1,10 +1,16 @@
+import {
+	MaxLength,
+	Min,
+	Max
+} from "class-validator";
+
 
 export class UpdateProductDto {
-  category: string;
-  subcategory: string;
-  name: string;
-  price: number;
+  @MaxLength(50) category: string;
+  @MaxLength(50) subcategory: string;
+  @MaxLength(50) name: string;
+  @Min(0) @Max(1000000) price: number;
+  @Min(0) @Max(1000000) amount: number;
   available: boolean;
-  amount: number;
   release: Date;
 }
