707e6c62ca525b6f738dba53effe3bc4696f2a23
Reinaldo Salla <reinaldosalla95@gmail.com>
2020-03-21 11:19:45 -0300

    Added validations for POST method

--- backend/src/products/dto/create-product.dto.ts
+++ backend/src/products/dto/create-product.dto.ts
@@ -6,10 +6,10 @@ import {
 } from "class-validator";
 
 export class CreateProductDto {
-  category: string;
-  subcategory: string;
-  name: string;
-  price: number;
+  @MinLength(5) @MaxLength(10) category: string;
+  @MinLength(5) @MaxLength(10) subcategory: string;
+  @MinLength(5) @MaxLength(10) name: string;
+  @Min(1) @Max(100) price: number;
   available: boolean;
   amount: number;
   release: Date;
